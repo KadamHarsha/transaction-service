f8b928341c01cb4a53d4616a0a56203917b44b5c
Harsha Vithoba Kadam <harshavk@maveric-systems.com>
2022-11-11 16:00:58 +0530

    New changes for happy path

--- src/main/java/com/transaction/transactionservice/controller/TransactionController.java
+++ src/main/java/com/transaction/transactionservice/controller/TransactionController.java
@@ -13,10 +13,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class TransactionController {
-   @Autowired
+    @Autowired
     TransactionService transactionService;
 
-       /*create transaction*/
+    /* public TransactionController(TransactionService transactionService) {
+         this.transactionService = transactionService;
+     }*/
+    /*List of total transaction*/
+    @GetMapping("accounts/{accountId}/transactions")
+    public ResponseEntity<List<TransactionDto>> getTransactions(@PathVariable String accountId, @RequestParam(defaultValue = "0") Integer page,
+                                                                @RequestParam(defaultValue = "10") Integer pageSize) {
+        List<TransactionDto> transactionDtoResponse = transactionService.getTransactions();
+        return new ResponseEntity<List<TransactionDto>>(transactionDtoResponse, HttpStatus.OK);
+    }
+
+    /*List of transaction using transaction ID*/
+    @GetMapping("accounts/{accountId}/transactions/{transactionId}")
+    public ResponseEntity<TransactionDto> getTransactionDetails(@PathVariable String accountId, @PathVariable String transactionId) {
+        TransactionDto transactionDtoResponse = transactionService.getTransactionById(transactionId);
+        return new ResponseEntity<TransactionDto>(transactionDtoResponse, HttpStatus.OK);
+    }
+
+    /*delete transaction based on transaction id*/
+    @DeleteMapping("accounts/{accountId}/transactions/{transactionId}")
+    public ResponseEntity<String> deleteTransaction(@PathVariable String accountId,@PathVariable String transactionId) {
+        String result = transactionService.deleteTransaction(transactionId);
+        return new ResponseEntity<String>(result, HttpStatus.OK);
+    }
+
+    /*create transaction*/
     @PostMapping("accounts/{accountId}/transactions")
     public ResponseEntity<TransactionDto> createTransaction(@PathVariable String accountId, @RequestBody TransactionDto transactionDto) {
         System.out.println("-----create Transaction 1-----");
