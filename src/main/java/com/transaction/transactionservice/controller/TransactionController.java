e811a8dbece22c3ec80573f2df090614f47bf221
Harsha Vithoba Kadam <harshavk@maveric-systems.com>
2022-11-11 15:32:26 +0530

    Delete transaction happy path

--- src/main/java/com/transaction/transactionservice/controller/TransactionController.java
+++ src/main/java/com/transaction/transactionservice/controller/TransactionController.java
@@ -16,24 +16,6 @@ public class TransactionController {
    @Autowired
     TransactionService transactionService;
 
-   /* public TransactionController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }*/
-    /*List of total transaction*/
-    @GetMapping("accounts/{accountId}/transactions")
-    public ResponseEntity<List<TransactionDto>> getTransactions(@PathVariable String accountId, @RequestParam(defaultValue = "0") Integer page,
-                                                                @RequestParam(defaultValue = "10") Integer pageSize) {
-        List<TransactionDto> transactionDtoResponse = transactionService.getTransactions();
-        return new ResponseEntity<List<TransactionDto>>(transactionDtoResponse, HttpStatus.OK);
-    }
-
-    /*List of transaction using transaction ID*/
-    @GetMapping("accounts/{accountId}/transactions/{transactionId}")
-    public ResponseEntity<TransactionDto> getTransactionDetails(@PathVariable String accountId, @PathVariable String transactionId) {
-        TransactionDto transactionDtoResponse = transactionService.getTransactionById(transactionId);
-        return new ResponseEntity<TransactionDto>(transactionDtoResponse, HttpStatus.OK);
-    }
-
     /*delete transaction based on transaction id*/
     @DeleteMapping("accounts/{accountId}/transactions/{transactionId}")
     public ResponseEntity<String> deleteTransaction(@PathVariable String accountId,@PathVariable String transactionId) {
@@ -41,12 +23,4 @@ public class TransactionController {
         return new ResponseEntity<String>(result, HttpStatus.OK);
     }
 
-    /*create transaction*/
-    @PostMapping("accounts/{accountId}/transactions")
-    public ResponseEntity<TransactionDto> createTransaction(@PathVariable String accountId, @RequestBody TransactionDto transactionDto) {
-        System.out.println("-----create Transaction 1-----");
-        TransactionDto transactionDtoResponse = transactionService.createTransaction(transactionDto);
-        System.out.println("-----create Transaction 2-----");
-        return new ResponseEntity<TransactionDto>(transactionDtoResponse, HttpStatus.OK);
-    }
 }
